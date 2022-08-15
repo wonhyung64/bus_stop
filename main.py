4190395dfdb49f973ed461b211971d6d6e27799f
an-seunghwan <wonhyung64@icloud.com>
2022-08-15 16:49:04 +0900

    feat - add statsitics

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #%%
+from tkinter.tix import COLUMN
 import matplotlib.pyplot as plt
 import pandas as pd
 from utils import (
@@ -67,19 +68,29 @@ def construct_pop_info():
 
 
 def EDA(data_dir, smpl_SeungDong, periods, bool_shelter):
-    for per_bef, per_aft, build_day in periods:
+    for num, (per_bef, per_aft, build_day) in enumerate(periods):
         if bool_shelter: 
             str_shelter = "쉼터O"
+            int_shelter = 2
             shelters = load_shelters_lst(data_dir, f"스마트쉼터_{build_day}")
             shelter_smpl = smpl_SeungDong[smpl_SeungDong["버스정류장ARS번호_Text"].isin(shelters)]
         else: 
             str_shelter = "쉼터X"
+            int_shelter = 1
             no_shelters_lst = load_shelters_lst(data_dir, "스마트쉼터")
             shelter_smpl = smpl_SeungDong[
                 ~smpl_SeungDong["버스정류장ARS번호_Text"].isin(no_shelters_lst)
             ]
 
         mu_1, mu_2 = extract_mean_pair(shelter_smpl, per_bef, per_aft)
+        stats1 = pd.Series(mu_1).describe().to_frame(
+            name=f"{per_bef[0][:4]}.{per_bef[0][4:6]}-{per_bef[1][:4]}.{per_bef[1][4:6]}"
+            )
+        stats2 = pd.Series(mu_2).describe().to_frame(
+            name=f"{per_aft[0][:4]}.{per_aft[0][4:6]}-{per_aft[1][:4]}.{per_aft[1][4:6]}"
+            )
+        stats = pd.concat([stats1, stats2], axis=1)
+        stats.to_csv(f"C:/Users/USER/Documents/GitHub/bus_stop/data/res/eda{int_shelter}_{num+1}.csv", index=True)
         n = len(mu_1)
         draw_boxplot(mu_1, mu_2, build_day, per_bef, per_aft, str_shelter, n)
 
@@ -119,4 +130,5 @@ if __name__ == "__main__":
     )
     smpl_SeungDong = df[df["구명칭"] == "성동구"]
     periods = mk_periods()
-    EDA(data_dir, smpl_SeungDong, periods, bool_shelter=True)
+    EDA(data_dir, smpl_SeungDong, periods, bool_shelter=False)
+    
